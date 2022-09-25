68b1ea6a51edd64c0691095674238def834e8779
john-mirage <chaosdynamix@posteo.net>
2022-09-25 07:12:21 +0200

    feat: add basic api

--- src/api/job-api.ts
+++ src/api/job-api.ts
@@ -1,6 +1,48 @@
+import jobs from "@data/jobs.json";
 
+interface HTMLElementWithJobFilters extends HTMLElement {
+  jobFilters: Set<string>;
+};
 
 class JobApi {
+  #jobFiltersSubscribers: Map<string, HTMLElementWithJobFilters> = new Map();
+  jobs: Map<number, AppData.Job> = new Map();
+  jobFilters: Set<string> = new Set();
 
+  constructor(jobs: AppData.Job[]) {
+    jobs.forEach((job) => {
+      this.jobs.set(job.id, job);
+    });
+  }
+
+  addJobFilter(filter: string) {
+    this.jobFilters.add(filter);
+    this.dispatch();
+  }
+
+  deleteJobFilter(filter: string) {
+    this.jobFilters.delete(filter);
+    this.dispatch();
+  }
+
+  clearJobFilter() {
+    this.jobFilters.clear();
+    this.dispatch();
+  }
+
+  dispatch() {
+    this.#jobFiltersSubscribers.forEach((jobFilterSubscriber) => {
+      jobFilterSubscriber.jobFilters = this.jobFilters;
+    });
+  }
+
+  subscribe(elementName: string, element: HTMLElementWithJobFilters) {
+    this.#jobFiltersSubscribers.set(elementName, element);
+  }
+
+  unsubscribe(elementName: string) {
+    this.#jobFiltersSubscribers.delete(elementName);
+  }
 }
 
+export default new JobApi(jobs);
